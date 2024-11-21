d0e88bff40c782425133e1c8bf69ab4bc7beb639
digitalscyther <digitalscyther@gmail.com>
2024-11-21 18:05:59 +0800

    fix - zzzzZ

--- web/src/components/LocalDate.js
+++ web/src/components/LocalDate.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LocalDate = ({date}) => {
     const formatDateTime = (dateString) => {
         if (!dateString) return 'N/A';
-        return new Date(dateString + "z").toLocaleString();
+        return new Date(dateString + "Z").toLocaleString();
     };
 
     return (
