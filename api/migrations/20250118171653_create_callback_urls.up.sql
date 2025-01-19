b87ddbc02564c877774bb39e11ac9d9628e13613
digitalscyther <digitalscyther@gmail.com>
2025-01-19 20:01:22 +0800

    add - frontend for whitelisted callback_urls

--- api/migrations/20250118171653_create_callback_urls.up.sql
+++ api/migrations/20250118171653_create_callback_urls.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE callback_urls (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
-    url TEXT NOT NULL UNIQUE,
+    url TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
